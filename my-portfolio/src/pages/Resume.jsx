0ebf405f75dc7d84ee20632f3ca953210fbebd07
Andrew <pelfreya6@gmail.com>
2025-02-24 21:10:32 -0500

    resume

--- my-portfolio/src/pages/Resume.jsx
+++ my-portfolio/src/pages/Resume.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Resume.css";
-import resumePDF from "/assets/Andrew_Pelfrey_Developer_Resume.pdf";
+import resumePDF from "/assets/Andrew_Pelfrey_-_Software_Developer.pdf";
 import profileImage from "/assets/portfolio-image.jpg";
 
  const Resume = () => {
